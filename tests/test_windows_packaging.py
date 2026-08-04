dc35d51e3b45351fe177813e260b30ab87dad92c
Wiktor Baura <wbaura@dataplicity.com>
2026-08-04 13:12:17 +0200

    fix x64 MSI installation path

--- tests/test_windows_packaging.py
+++ tests/test_windows_packaging.py
@@ -10,6 +10,18 @@ WIX_NAMESPACE = {"wix": "http://schemas.microsoft.com/wix/2006/wi"}
 
 
 class WindowsPackagingTest(unittest.TestCase):
+    def test_msi_targets_64_bit_program_files(self) -> None:
+        root = ET.parse(WIX_SOURCE).getroot()
+        package = root.find(".//wix:Package", WIX_NAMESPACE)
+        install_root = root.find(".//wix:Directory[@Id='ProgramFiles64Folder']", WIX_NAMESPACE)
+        executable_component = root.find(".//wix:Component[@Id='MainExecutable']", WIX_NAMESPACE)
+
+        self.assertIsNotNone(package)
+        self.assertEqual(package.get("Platform"), "x64")
+        self.assertIsNotNone(install_root)
+        self.assertIsNotNone(executable_component)
+        self.assertEqual(executable_component.get("Win64"), "yes")
+
     def test_msi_embeds_its_cabinet(self) -> None:
         root = ET.parse(WIX_SOURCE).getroot()
         media_template = root.find(".//wix:MediaTemplate", WIX_NAMESPACE)
